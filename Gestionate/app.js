e0abd9fcf7a46b7969cf68347e4dbf3ac956f91a
xMatee <110862698+xMatee@users.noreply.github.com>
2023-10-12 02:05:57 -0300

    cambios

--- Gestionate/app.js
+++ Gestionate/app.js
@@ -4,7 +4,8 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
+import dotenv from 'dotenv'
+dotenv.config();
 // Pass --options via CLI arguments in command to enable these options.
 export const options = {}
 
