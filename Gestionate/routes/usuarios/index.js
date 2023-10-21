ea56be5896a941750d8f1bec1821d188b345fb73
xMatee <110862698+xMatee@users.noreply.github.com>
2023-10-21 15:29:58 -0300

    vuelta atras en main

--- Gestionate/routes/usuarios/index.js
+++ Gestionate/routes/usuarios/index.js
@@ -8,17 +8,6 @@ import {
     updateUserQuery,
     deleteUserQuery
 } from "../../DB/queries/users.js";
-import {
-    getAllGastosQuery,
-    getGastoByIdQuery,
-    insertGastoQuery,
-    updateGastoQuery,
-    deleteGastoQuery,
-} from "../../DB/queries/gastos.js";
-import {
-    associateProductoToGastoQuery,
-    disassociateProductoFromGastoQuery,
-} from "../../DB/queries/productos.js";
 
 export default async function (fastify, opts) {
     // Obtener Todos los usuarios
@@ -87,98 +76,6 @@ export default async function (fastify, opts) {
             reply.status(204).send();
         } catch (error) {
             console.error("Error al eliminar usuario", error.message);
-            reply.status(500).send("Error del servidor");
-        }
-    });
-
-    // Obtener todos los gastos de un usuario
-    fastify.get("/:usuario_id/gastos", async function (request, reply) {
-        const { usuario_id } = request.params;
-        try {
-            const res = await query(getAllGastosQuery, [usuario_id]);
-            return res.rows;
-        } catch (error) {
-            console.error("Error al obtener gastos", error.message);
-            reply.status(500).send("Error del servidor");
-        }
-    });
-
-    // Obtener un gasto de un usuario por su ID
-    fastify.get("/:usuario_id/gastos/:id", async function (request, reply) {
-        const { usuario_id, id } = request.params;
-        try {
-            const res = await query(getGastoByIdQuery, [usuario_id, id]);
-            return res.rows[0];
-        } catch (error) {
-            console.error("Error al obtener el gasto", error.message);
-            reply.status(500).send("Error del servidor");
-        }
-    });
-
-    // Crear un nuevo gasto para un usuario
-    fastify.post("/:usuario_id/gastos", { schema: schemas.createExpenseSchema }, async function (request, reply) {
-        const { cantidad, fecha, descripcion, categoria_id, subcategoria_id } = request.body;
-        const { usuario_id } = request.params;
-        try {
-            const res = await query(insertGastoQuery, [cantidad, fecha, descripcion, categoria_id, subcategoria_id, usuario_id]);
-            reply.code(201);
-            return res.rows[0];
-        } catch (error) {
-            console.error("Error al crear el gasto", error.message);
-            reply.status(500).send("Error del servidor");
-        }
-    });
-
-    // Actualizar un gasto de un usuario por su ID
-    fastify.put("/:usuario_id/gastos/:id", { schema: schemas.editExpenseSchema }, async function (request, reply) {
-        const { usuario_id, id } = request.params;
-        const { cantidad, fecha, descripcion, categoria_id, subcategoria_id } = request.body;
-        try {
-            const res = await query(updateGastoQuery, [cantidad, fecha, descripcion, categoria_id, subcategoria_id, usuario_id, id]);
-            return res.rows[0];
-        } catch (error) {
-            console.error("Error al actualizar el gasto", error.message);
-            reply.status(500).send("Error del servidor");
-        }
-    });
-
-    // Eliminar un gasto de un usuario por su ID
-    fastify.delete("/:usuario_id/gastos/:id", { schema: schemas.expenseDeletedResponseSchema }, async function (request, reply) {
-        const { usuario_id, id } = request.params;
-        try {
-            const res = await query(deleteGastoQuery, [usuario_id, id]);
-            reply.code(204);
-            return res.rows[0];
-        } catch (error) {
-            console.error("Error al eliminar el gasto", error.message);
-            reply.status(500).send("Error del servidor");
-        }
-    });
-
-    // PRODUCTOS ASOCIADOS A GASTOS
-
-    // ASOCIAR PRODUCTO A UN GASTO DE UN USUARIO
-    fastify.post("/:usuario_id/gastos/:gasto_id/productos/:producto_id", async (request, reply) => {
-        const { usuario_id, gasto_id, producto_id } = request.params;
-        try {
-            const res = await query(associateProductoToGastoQuery, [gasto_id, producto_id]);
-            reply.code(201);
-            return res.rows[0];
-        } catch (error) {
-            console.error("Error al asociar producto a gasto", error.message);
-            reply.status(500).send("Error del servidor");
-        }
-    });
-
-    // DESASOCIAR PRODUCTO DE UN GASTO DE UN USUARIO
-    fastify.delete("/:usuario_id/gastos/:gasto_id/productos/:producto_id", async (request, reply) => {
-        const { usuario_id, gasto_id, producto_id } = request.params;
-        try {
-            const res = await query(disassociateProductoFromGastoQuery, [gasto_id, producto_id]);
-            reply.code(204);
-            return res.rows[0];
-        } catch (error) {
-            console.error("Error al desasociar producto de gasto", error.message);
             reply.status(500).send("Error del servidor");
         }
     });
