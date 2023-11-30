1204245ad3e96a4818c828e62d4c470ef8a8fa73
xMatee <110862698+xMatee@users.noreply.github.com>
2023-11-30 13:34:58 -0300

    Update auth-guard.guard.ts

--- FrontEnd/src/app/auth/guards/auth-guard.guard.ts
+++ FrontEnd/src/app/auth/guards/auth-guard.guard.ts
@@ -1,8 +1,8 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateChildFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuardGuard: CanActivateFn = (route, state) => {
+export const authGuardGuard: CanActivateChildFn = (route, state) => {
   const authService = inject(AuthService)
   const router = inject(Router)
   const isAuth = authService.isActive()
